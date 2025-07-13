b9c2e8da4e8a3b9e022630fcb5ba0ae7e833cffd
sakshisali <salisakshi868@gmail.com>
2025-07-13 12:14:30 +0000

    datatypes

---
+++ 01_Basics/02_Datatype.js
@@ -0,0 +1,24 @@
+// "Use Strict"; //treat all js code as newer version
+// alert(3+3) //we are using nodejs not browser
+
+
+let name = "sakshi" //string
+let age = 22 // number
+let isLogedIn = true // boolean
+
+
+
+//primitive data types//
+
+/* number = 2 to power 53
+   bigint
+   string = ""
+   boolean = true/false
+   null = standalone value
+   undefined = 
+   symbol = unique*/
+
+   //object 
+
+   console.log(typeof "sakshi");
+   console.log("name");
