b5f86e937917daf3c4f320fd991725c204c68bc1
sakshisali <salisakshi868@gmail.com>
2025-07-13 13:51:42 +0000

    conversionoperation

---
+++ 01_Basics/03_conversionoperation.js
@@ -0,0 +1,28 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" = 33
+// 33abc = NaN
+// true = 1 false =0
+
+let isLoggedIn = "sakshi"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+//1 = true 
+//0 = false
+// sakshi = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
