664505b299973f2e5a425c46ab6e6d025ef7b360
sakshisali <salisakshi868@gmail.com>
2025-07-14 14:07:58 +0000

    stack heap

---
+++ 01_Basics/05_Stack_heap.js
@@ -0,0 +1,23 @@
+//stack (primitive) heap (non-primitive)//
+
+let myYoutubename    = "sakshidotcom"
+let anothername = myYoutubename
+console.log(anothername)
+
+anothername = "chaiaurcode"
+console.log(myYoutubename)
+console.log(anothername)
+
+let userOne = {
+    email : "sakshi@gmail.com"
+    
+}
+
+let userTwo = userOne
+userTwo.email = "sakshi@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
+
+
