14b03236b0bf15ef0a97ae264108e94115a0af68
sakshisali <salisakshi868@gmail.com>
2025-07-14 10:15:23 +0000

    datatype summary

---
+++ 01_Basics/04_Datatypes-summary.js
@@ -0,0 +1,22 @@
+//primitive data types//
+
+//srting, number,null,boolean,symbol,bigInt, undefined//
+
+//Reference (Non-primitive data types)//
+
+//Array, Object, function//
+
+const fruits = ["mango," ,"banana", "grapes"];
+console.log(fruits);
+
+let myobj ={
+    name : "sakshi",
+    age: 23
+
+
+}
+console.log(myobj)
+
+const myfunction = function(){
+
+}
