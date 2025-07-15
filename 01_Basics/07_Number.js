f3731a2f239c5725f27a4977fac5edd013ae88f9
sakshisali <salisakshi868@gmail.com>
2025-07-15 12:34:47 +0000

    numbers

--- 01_Basics/07_Number.js
+++ 01_Basics/07_Number.js
@@ -34,3 +34,15 @@ console.log(Math.random())
 const max = 20
 
 console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toDateString())
+
+let myCreatedDate = new Date()
+console.log(myCreatedDate.toDateString(2025,7,15))
+
+
+
+
