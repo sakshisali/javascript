ca1598f7901a16dc655715649ab275998777b54f
sakshisali <salisakshi868@gmail.com>
2025-07-15 13:58:27 +0000

    object

---
+++ 01_Basics/09_Object.js
@@ -0,0 +1,17 @@
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email)
+console.log(JsUser["email"])
+console.log(JsUser["full name"])
+
+
+
