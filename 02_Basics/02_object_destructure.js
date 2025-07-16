5ec1175beae06eca574c4e1e3d0728b185db0a79
sakshisali <salisakshi868@gmail.com>
2025-07-16 09:03:33 +0000

    json

---
+++ 02_Basics/02_object_destructure.js
@@ -0,0 +1,20 @@
+const course ={
+    coursename: "js in hindi",
+    price: 999,
+    courseInstructor : "hitesh"
+
+}
+
+const {price:instructor } = course
+
+console.log(instructor);
+
+console.log(course)
+
+//JSON// 
+
+{
+    "name": "hitesh",
+    "coursename": "js in hindi",
+    "price": "free"
+}
